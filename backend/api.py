f9ce9f35916562856d31c1e6f10b9e27fd77f1d4
powoftech <powoftech@gmail.com>
2024-05-05 23:07:09 +0700

    fix: update imports

--- backend/api.py
+++ backend/api.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, UploadFile, File, Body
-from fastapi.responses import JSONResponse
-from PIL import Image
 import io
-import numpy as np
+
 import cv2
 import model
+import numpy as np
+from fastapi import Body, FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
 
 app = FastAPI()
 
 ############# IMAGE FILE TEST - USING POSTMAN
 # @app.post("/predict/")
 # async def predict_api(file: UploadFile = File(...)):
-    
+
 #     image_data = await file.read()
 #     image = Image.open(io.BytesIO(image_data))
 
@@ -22,10 +23,11 @@ app = FastAPI()
 
 #     return JSONResponse(content=prediction)
 
+
 ############# IMAGE BYTE TEST
 @app.post("/predict/")
 async def predict_api(image_data: bytes = Body(...)):
-    
+
     image = Image.open(io.BytesIO(image_data))
 
     image = np.array(image)
